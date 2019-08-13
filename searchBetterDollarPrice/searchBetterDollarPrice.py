93ea74b1637836e67bb67743f2dd19787344bd3e
juan.cuello <juan.cuello@wnco.com>
2019-08-13 07:45:57 -0500

    tool to search the better dollar price

---
+++ searchBetterDollarPrice/searchBetterDollarPrice.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import webbrowser
+
+url1 = "http://www.bullexchange.com.ar/"
+url2 = "http://www.dolarhoy.com/"
+url3 = "https://www.cronista.com/MercadosOnline/"
+url4 = "https://www.balanz.com/dolares"
+
+webbrowser.open_new_tab(url1)
+webbrowser.open_new_tab(url2)
+webbrowser.open_new_tab(url3)
+webbrowser.open_new_tab(url4)
